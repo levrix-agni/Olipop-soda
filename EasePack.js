7edab7e1e25875d9ad7c82204b5739dc8026ee5b
levrix-agni <agnichowdhury2006@gmail.com>
2026-05-17 01:04:31 +0530

    Add files via upload

--- EasePack.js
+++ EasePack.js
@@ -1,216 +1,162 @@
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(global = global || self, factory(global.window = global.window || {}));
-}(this, (function (exports) { 'use strict';
+/*!
+ * EasePack 3.15.0
+ * https://gsap.com
+ *
+ * @license Copyright 2008-2026, GreenSock. All rights reserved.
+ * Subject to the terms at https://gsap.com/standard-license
+ * @author: Jack Doyle, jack@greensock.com
+*/
+/* eslint-disable */
 
-	/*!
-	 * EasePack 3.15.0
-	 * https://gsap.com
-	 *
-	 * @license Copyright 2008-2026, GreenSock. All rights reserved.
-	 * Subject to the terms at https://gsap.com/standard-license
-	 * @author: Jack Doyle, jack@greensock.com
-	*/
-	var gsap,
-	    _registerEase,
-	    _getGSAP = function _getGSAP() {
-	  return gsap || typeof window !== "undefined" && (gsap = window.gsap) && gsap.registerPlugin && gsap;
+let gsap, _coreInitted, _registerEase,
+	_getGSAP = () => gsap || (typeof(window) !== "undefined" && (gsap = window.gsap) && gsap.registerPlugin && gsap),
+	_boolean = (value, defaultValue) => !!(typeof(value) === "undefined" ? defaultValue : value && !~((value + "").indexOf("false"))),
+	_initCore = core => {
+		gsap = core || _getGSAP();
+		if (gsap) {
+			_registerEase = gsap.registerEase;
+			//add weighted ease capabilities to standard eases so users can do "power2.inOut(0.8)" for example to push everything toward the "out", or (-0.8) to push it toward the "in" (0 is neutral)
+			let eases = gsap.parseEase(),
+				createConfig = ease => ratio => {
+					let y = 0.5 + ratio / 2;
+					ease.config = p => ease(2 * (1 - p) * p * y + p * p);
+				},
+				p;
+			for (p in eases) {
+				if (!eases[p].config) {
+					createConfig(eases[p]);
+				}
+			}
+			_registerEase("slow", SlowMo);
+			_registerEase("expoScale", ExpoScaleEase);
+			_registerEase("rough", RoughEase);
+			for (p in EasePack) {
+				p !== "version" && gsap.core.globals(p, EasePack[p]);
+			}
+			_coreInitted = 1;
+		}
 	},
-	    _boolean = function _boolean(value, defaultValue) {
-	  return !!(typeof value === "undefined" ? defaultValue : value && !~(value + "").indexOf("false"));
+	_createSlowMo = (linearRatio, power, yoyoMode) => {
+		linearRatio = Math.min(1, linearRatio || 0.7);
+		let pow = linearRatio < 1 ? ((power || power === 0) ? power : 0.7) : 0,
+			p1 = (1 - linearRatio) / 2,
+			p3 = p1 + linearRatio,
+			calcEnd = _boolean(yoyoMode);
+		return p => {
+			let r = p + (0.5 - p) * pow;
+			return (p < p1) ? (calcEnd ? 1 - ((p = 1 - (p / p1)) * p) : r - ((p = 1 - (p / p1)) * p * p * p * r)) : (p > p3) ? (calcEnd ? (p === 1 ? 0 : 1 - (p = (p - p3) / p1) * p) : r + ((p - r) * (p = (p - p3) / p1) * p * p * p)) : (calcEnd ? 1 : r);
+		}
 	},
-	    _initCore = function _initCore(core) {
-	  gsap = core || _getGSAP();
-
-	  if (gsap) {
-	    _registerEase = gsap.registerEase;
-
-	    var eases = gsap.parseEase(),
-	        createConfig = function createConfig(ease) {
-	      return function (ratio) {
-	        var y = 0.5 + ratio / 2;
-
-	        ease.config = function (p) {
-	          return ease(2 * (1 - p) * p * y + p * p);
-	        };
-	      };
-	    },
-	        p;
-
-	    for (p in eases) {
-	      if (!eases[p].config) {
-	        createConfig(eases[p]);
-	      }
-	    }
-
-	    _registerEase("slow", SlowMo);
-
-	    _registerEase("expoScale", ExpoScaleEase);
-
-	    _registerEase("rough", RoughEase);
-
-	    for (p in EasePack) {
-	      p !== "version" && gsap.core.globals(p, EasePack[p]);
-	    }
-	  }
+	_createExpoScale = (start, end, ease) => {
+		let p1 = Math.log(end / start),
+			p2 = end - start;
+		ease && (ease = gsap.parseEase(ease));
+		return p => (start * Math.exp(p1 * (ease ? ease(p) : p)) - start) / p2;
 	},
-	    _createSlowMo = function _createSlowMo(linearRatio, power, yoyoMode) {
-	  linearRatio = Math.min(1, linearRatio || 0.7);
-
-	  var pow = linearRatio < 1 ? power || power === 0 ? power : 0.7 : 0,
-	      p1 = (1 - linearRatio) / 2,
-	      p3 = p1 + linearRatio,
-	      calcEnd = _boolean(yoyoMode);
-
-	  return function (p) {
-	    var r = p + (0.5 - p) * pow;
-	    return p < p1 ? calcEnd ? 1 - (p = 1 - p / p1) * p : r - (p = 1 - p / p1) * p * p * p * r : p > p3 ? calcEnd ? p === 1 ? 0 : 1 - (p = (p - p3) / p1) * p : r + (p - r) * (p = (p - p3) / p1) * p * p * p : calcEnd ? 1 : r;
-	  };
+	EasePoint = function(time, value, next) {
+		this.t = time;
+		this.v = value;
+		if (next) {
+			this.next = next;
+			next.prev = this;
+			this.c = next.v - value;
+			this.gap = next.t - time;
+		}
 	},
-	    _createExpoScale = function _createExpoScale(start, end, ease) {
-	  var p1 = Math.log(end / start),
-	      p2 = end - start;
-	  ease && (ease = gsap.parseEase(ease));
-	  return function (p) {
-	    return (start * Math.exp(p1 * (ease ? ease(p) : p)) - start) / p2;
-	  };
-	},
-	    EasePoint = function EasePoint(time, value, next) {
-	  this.t = time;
-	  this.v = value;
-
-	  if (next) {
-	    this.next = next;
-	    next.prev = this;
-	    this.c = next.v - value;
-	    this.gap = next.t - time;
-	  }
-	},
-	    _createRoughEase = function _createRoughEase(vars) {
-	  if (typeof vars !== "object") {
-	    vars = {
-	      points: +vars || 20
-	    };
-	  }
-
-	  var taper = vars.taper || "none",
-	      a = [],
-	      cnt = 0,
-	      points = (+vars.points || 20) | 0,
-	      i = points,
-	      randomize = _boolean(vars.randomize, true),
-	      clamp = _boolean(vars.clamp),
-	      template = gsap ? gsap.parseEase(vars.template) : 0,
-	      strength = (+vars.strength || 1) * 0.4,
-	      x,
-	      y,
-	      bump,
-	      invX,
-	      obj,
-	      pnt,
-	      recent;
-
-	  while (--i > -1) {
-	    x = randomize ? Math.random() : 1 / points * i;
-	    y = template ? template(x) : x;
-
-	    if (taper === "none") {
-	      bump = strength;
-	    } else if (taper === "out") {
-	      invX = 1 - x;
-	      bump = invX * invX * strength;
-	    } else if (taper === "in") {
-	      bump = x * x * strength;
-	    } else if (x < 0.5) {
-	      invX = x * 2;
-	      bump = invX * invX * 0.5 * strength;
-	    } else {
-	      invX = (1 - x) * 2;
-	      bump = invX * invX * 0.5 * strength;
-	    }
-
-	    if (randomize) {
-	      y += Math.random() * bump - bump * 0.5;
-	    } else if (i % 2) {
-	      y += bump * 0.5;
-	    } else {
-	      y -= bump * 0.5;
-	    }
-
-	    if (clamp) {
-	      if (y > 1) {
-	        y = 1;
-	      } else if (y < 0) {
-	        y = 0;
-	      }
-	    }
-
-	    a[cnt++] = {
-	      x: x,
-	      y: y
-	    };
-	  }
-
-	  a.sort(function (a, b) {
-	    return a.x - b.x;
-	  });
-	  pnt = new EasePoint(1, 1, null);
-	  i = points;
-
-	  while (i--) {
-	    obj = a[i];
-	    pnt = new EasePoint(obj.x, obj.y, pnt);
-	  }
-
-	  recent = new EasePoint(0, 0, pnt.t ? pnt : pnt.next);
-	  return function (p) {
-	    var pnt = recent;
-
-	    if (p > pnt.t) {
-	      while (pnt.next && p >= pnt.t) {
-	        pnt = pnt.next;
-	      }
-
-	      pnt = pnt.prev;
-	    } else {
-	      while (pnt.prev && p <= pnt.t) {
-	        pnt = pnt.prev;
-	      }
-	    }
-
-	    recent = pnt;
-	    return pnt.v + (p - pnt.t) / pnt.gap * pnt.c;
-	  };
+	_createRoughEase = vars => {
+		if (typeof(vars) !== "object") { //users may pass in via a string, like "rough(30)"
+			vars = {points: +vars || 20};
+		}
+		let taper = vars.taper || "none",
+			a = [],
+			cnt = 0,
+			points = (+vars.points || 20) | 0,
+			i = points,
+			randomize = _boolean(vars.randomize, true),
+			clamp = _boolean(vars.clamp),
+			template = gsap ? gsap.parseEase(vars.template) : 0,
+			strength = (+vars.strength || 1) * 0.4,
+			x, y, bump, invX, obj, pnt, recent;
+		while (--i > -1) {
+			x = randomize ? Math.random() : (1 / points) * i;
+			y = template ? template(x) : x;
+			if (taper === "none") {
+				bump = strength;
+			} else if (taper === "out") {
+				invX = 1 - x;
+				bump = invX * invX * strength;
+			} else if (taper === "in") {
+				bump = x * x * strength;
+			} else if (x < 0.5) {  //"both" (start)
+				invX = x * 2;
+				bump = invX * invX * 0.5 * strength;
+			} else {				//"both" (end)
+				invX = (1 - x) * 2;
+				bump = invX * invX * 0.5 * strength;
+			}
+			if (randomize) {
+				y += (Math.random() * bump) - (bump * 0.5);
+			} else if (i % 2) {
+				y += bump * 0.5;
+			} else {
+				y -= bump * 0.5;
+			}
+			if (clamp) {
+				if (y > 1) {
+					y = 1;
+				} else if (y < 0) {
+					y = 0;
+				}
+			}
+			a[cnt++] = {x:x, y:y};
+		}
+		a.sort((a, b) => a.x - b.x);
+		pnt = new EasePoint(1, 1, null);
+		i = points;
+		while (i--) {
+			obj = a[i];
+			pnt = new EasePoint(obj.x, obj.y, pnt);
+		}
+		recent = new EasePoint(0, 0, pnt.t ? pnt : pnt.next);
+		return p => {
+			let pnt = recent;
+			if (p > pnt.t) {
+				while (pnt.next && p >= pnt.t) {
+					pnt = pnt.next;
+				}
+				pnt = pnt.prev;
+			} else {
+				while (pnt.prev && p <= pnt.t) {
+					pnt = pnt.prev;
+				}
+			}
+			recent = pnt;
+			return pnt.v + ((p - pnt.t) / pnt.gap) * pnt.c;
+		};
 	};
 
-	var SlowMo = _createSlowMo(0.7);
-	SlowMo.ease = SlowMo;
-	SlowMo.config = _createSlowMo;
-	var ExpoScaleEase = _createExpoScale(1, 2);
-	ExpoScaleEase.config = _createExpoScale;
-	var RoughEase = _createRoughEase();
-	RoughEase.ease = RoughEase;
-	RoughEase.config = _createRoughEase;
-	var EasePack = {
-	  SlowMo: SlowMo,
-	  RoughEase: RoughEase,
-	  ExpoScaleEase: ExpoScaleEase
-	};
+export const SlowMo = _createSlowMo(0.7);
+SlowMo.ease = SlowMo; //for backward compatibility
+SlowMo.config = _createSlowMo;
 
-	for (var p in EasePack) {
-	  EasePack[p].register = _initCore;
-	  EasePack[p].version = "3.15.0";
-	}
+export const ExpoScaleEase = _createExpoScale(1, 2);
+ExpoScaleEase.config = _createExpoScale;
 
-	_getGSAP() && gsap.registerPlugin(SlowMo);
+export const RoughEase = _createRoughEase();
+RoughEase.ease = RoughEase; //for backward compatibility
+RoughEase.config = _createRoughEase;
 
-	exports.EasePack = EasePack;
-	exports.ExpoScaleEase = ExpoScaleEase;
-	exports.RoughEase = RoughEase;
-	exports.SlowMo = SlowMo;
-	exports.default = EasePack;
+export const EasePack = {
+	SlowMo: SlowMo,
+	RoughEase: RoughEase,
+	ExpoScaleEase: ExpoScaleEase
+};
 
-	Object.defineProperty(exports, '__esModule', { value: true });
+for (let p in EasePack) {
+	EasePack[p].register = _initCore;
+	EasePack[p].version = "3.15.0";
+}
 
-})));
+_getGSAP() && gsap.registerPlugin(SlowMo);
+
+export { EasePack as default };
--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-import { gsap, Power0, Power1, Power2, Power3, Power4, Linear, Quad, Cubic, Quart, Quint, Strong, Elastic, Back, SteppedEase, Bounce, Sine, Expo, Circ, TweenLite, TimelineLite, TimelineMax } from "./gsap-core.js";
-import { CSSPlugin } from "./CSSPlugin.js";
-var gsapWithCSS = gsap.registerPlugin(CSSPlugin) || gsap,
-    // to protect from tree shaking
-TweenMaxWithCSS = gsapWithCSS.core.Tween;
-export { gsapWithCSS as gsap, gsapWithCSS as default, CSSPlugin, TweenMaxWithCSS as TweenMax, TweenLite, TimelineMax, TimelineLite, Power0, Power1, Power2, Power3, Power4, Linear, Quad, Cubic, Quart, Quint, Strong, Elastic, Back, SteppedEase, Bounce, Sine, Expo, Circ };
+import crypto from 'crypto'
+import { urlAlphabet } from './url-alphabet/index.js'
+const POOL_SIZE_MULTIPLIER = 128
+let pool, poolOffset
+let fillPool = bytes => {
+  if (!pool || pool.length < bytes) {
+    pool = Buffer.allocUnsafe(bytes * POOL_SIZE_MULTIPLIER)
+    crypto.randomFillSync(pool)
+    poolOffset = 0
+  } else if (poolOffset + bytes > pool.length) {
+    crypto.randomFillSync(pool)
+    poolOffset = 0
+  }
+  poolOffset += bytes
+}
+let random = bytes => {
+  fillPool((bytes |= 0))
+  return pool.subarray(poolOffset - bytes, poolOffset)
+}
+let customRandom = (alphabet, defaultSize, getRandom) => {
+  let mask = (2 << (31 - Math.clz32((alphabet.length - 1) | 1))) - 1
+  let step = Math.ceil((1.6 * mask * defaultSize) / alphabet.length)
+  return (size = defaultSize) => {
+    let id = ''
+    while (true) {
+      let bytes = getRandom(step)
+      let i = step
+      while (i--) {
+        id += alphabet[bytes[i] & mask] || ''
+        if (id.length === size) return id
+      }
+    }
+  }
+}
+let customAlphabet = (alphabet, size = 21) =>
+  customRandom(alphabet, size, random)
+let nanoid = (size = 21) => {
+  fillPool((size |= 0))
+  let id = ''
+  for (let i = poolOffset - size; i < poolOffset; i++) {
+    id += urlAlphabet[pool[i] & 63]
+  }
+  return id
+}
+export { nanoid, customAlphabet, customRandom, urlAlphabet, random }
